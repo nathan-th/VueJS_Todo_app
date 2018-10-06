5ed6bbd9e31773620448d1a8973a7b94dad26534
nathan-th <crayzlight722@gmail.com>
2018-10-06 19:59:43 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+<!-- Render the TodoList component
+     TodoList becomes-->
+
+  <!--We will need to pass data from the main component 
+  to the TodoList component. For this, we will use the v-bind 
+  directive. The directive takes an argument which is indicated by 
+  a colon after the directive name. Our argument will be todos which 
+  tells the v-bind directive to bind the element’s todos attribute to 
+  the value of the expression todos.-->
+    <todo-list v-bind:todos="todos"></todo-list>
+  </div>
+</template>
+
+<script>
+import TodoList from "./components/TodoList";
+
+export default {
+  name: "app",
+  components: {
+    TodoList
+  },
+
+  data() {
+    return {
+      todos: [
+        {
+          title: "Todo A",
+          project: "Project A",
+          done: false
+        },
+        {
+          title: "Todo B",
+          project: "Project B",
+          done: true
+        },
+        {
+          title: "Todo C",
+          project: "Project C"
+        },
+        {
+          title: "Todo D",
+          project: "Project D",
+          done: false
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,41 @@
+<template>
+
+    <div>
+        <ul>
+
+            <li> Todo A </li>
+            <li> Todo B </li>
+            <li> Todo C </li>
+
+        </ul>
+
+    </div>
+
+</template>
+
+
+<!-- We will need to supply data to the main component 
+that will be used to display the list of todos. Our todos 
+will have three properties; The title, project and done(to 
+indicate if the todo is complete or not). Components avail
+data to their respective templates using a data function. 
+This function returns an object with the properties intended 
+for the template. Let's add some data to our component. --> 
+
+
+
+<!-- The todos will now be available in the TodoList component as todos.
+We will have to modify our TodoList component to access this data. 
+The TodoList component has to declare the properties it will accept 
+when using it. We do this by adding a property to the component class. --> 
+
+<script type ="text/javascript">
+export default {
+  props: ["todos"]
+};
+</script>
+
+<style>
+</style>
+
+
